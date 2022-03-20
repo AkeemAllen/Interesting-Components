76209db332d4c8573e4e7720e32e708adc400f7f
Akeem <akeem@workstep.com>
2022-03-20 13:19:02 -0400

    added basic animations to starbutton but implementation likely needs work

--- components/StarButton/StarButton.tsx
+++ components/StarButton/StarButton.tsx
@@ -27,26 +27,56 @@ const StarButton = () => {
             <div className={styles.left}>
                 {!isStarred && (
                     <motion.div
-                        animate={{ scale: hover ? 1.5 : 1 }}
+                        animate={{ scale: hover ? 1.2 : 1 }}
                         className={styles.star}
                     >
                         <AiFillStar
                             size={30}
-                            color={hover ? "#f5f3a1" : "#797a83"}
+                            color={hover ? "#ffff2e" : "#797a83"}
                         />
                     </motion.div>
                 )}
-                <p>{isStarred ? "Starred" : "Star"}</p>
+                {!isStarred && (
+                    <motion.p
+                        animate={{ opacity: [0, 1], x: [-40, 0] }}
+                        transition={{ duration: 0.3 }}
+                    >
+                        Star
+                    </motion.p>
+                )}
+                {isStarred && (
+                    <motion.p
+                        animate={{ opacity: [0, 1], x: [40, 0] }}
+                        transition={{ duration: 0.3 }}
+                    >
+                        Starred
+                    </motion.p>
+                )}
             </div>
-            <motion.div>
-                <div
+            {/* <div> */}
+            {!isStarred && (
+                <motion.div
+                    animate={{ opacity: [0, 1], y: [-30, 0] }}
+                    transition={{ duration: 0.3, delay: 0.2 }}
                     className={`${styles.right}, ${
                         isStarred && styles.highlightColor
                     }`}
                 >
                     {count}
-                </div>
-            </motion.div>
+                </motion.div>
+            )}
+            {isStarred && (
+                <motion.div
+                    animate={{ opacity: [0, 1], y: [30, 0] }}
+                    transition={{ duration: 0.3, delay: 0.2 }}
+                    className={`${styles.right}, ${
+                        isStarred && styles.highlightColor
+                    }`}
+                >
+                    {count}
+                </motion.div>
+            )}
+            {/* </div> */}
         </motion.button>
     );
 };
